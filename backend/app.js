33608fd57436fc5113be410ccf95b151e0202dca
Jarle Aase <jarle.aase@macrometa.co>
2019-06-17 23:59:41 +0300

    Implemented distribution and reception of barfs

--- backend/app.js
+++ backend/app.js
@@ -2,50 +2,177 @@ const express = require('express')
 const bp = require('body-parser')
 const app = express()
 const axios = require('axios')
+const WebSocket = require('ws')
 
 axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.JWT}`
+const host = process.env.APIURL.replace(/(^\w+:|^)\/\//, '')
 
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
 
-app.post('/barfer/user', (req, res) => {
-    var body = req.body
+app.post('/barfer/user', async (req, res) => {
     console.log(`Request from ${req.connection.remoteAddress} to create user "${req.body.name}"`)
     
     // Create database user
-    axios.post(`${process.env.APIURL}/_tenant/${process.env.TENANT}/_fabric/${process.env.FABRIC}/_admin/user`, {
-        "active": true,
-        "extra": {},
-        "passwd": req.body.passwd,
-        "user": req.body.name
-    }).then(function (CreateUserRes) {
+    var doing = null;
+    try {
+        doing = 'Create user'
+        var CreateUserRes = await axios.post(
+            `${process.env.APIURL}/_tenant/${process.env.TENANT}/_fabric/${process.env.FABRIC}/_admin/user`, {
+            active: true,
+            extra: {},
+            passwd: req.body.passwd,
+            user: req.body.name
+        })
+    
+
         // Create a stream for the users feed
-        axios.post(`${process.env.APIURL}/_tenant/${process.env.TENANT}/_fabric/${process.env.FABRIC}/streams/persistent/stream/${req.body.name}`, {
-            outputTriggers: {}
-        }).then(function (CreateStreamRes) {
-            // Create a document in the 'users' collection for the user
-            axios.post(`${process.env.APIURL}/_tenant/${process.env.TENANT}/_fabric/${process.env.FABRIC}/document/users`, {
-                _key: req.body.name,
-                about: req.body.about,
-                avatar: req.body.avatar,
-                active: req.body.active
-            }).then(function (CreateDocRes) {
-                return res.status(202)
-                    .send(`Created user with id ${CreateDocRes.data._id}. Everything is OK!`);
-            }).catch(function (error) {
-                console.log(`*** Request from ${req.connection.remoteAddress} to create user doc "${req.body.name}" failed with error: ${error.message}`)
-                return res.status(500).send({what: 'Failed to create user doc', err: error.message})
-            });
-        }).catch(function (error) {
-            console.log(`*** Request from ${req.connection.remoteAddress} to create user stream "${req.body.name}" failed with error: ${error.message}`)
-            return res.status(500).send({what: 'Failed to create user stream', err: error.message})
-        });
-    }).catch(function (error) {
-        console.log(`*** Request from ${req.connection.remoteAddress} to create user "${req.body.name}" failed with error: ${error.message}`)
-        return res.status(500).send({what: 'Failed to create database user', err: error.message})
-    });
+        doing = 'Create stream'
+        CreateStreamRes = await axios.post(
+            `${process.env.APIURL}/_tenant/${process.env.TENANT}/_fabric/${process.env.FABRIC}/streams/persistent/stream/${req.body.name}`, {
+                outputTriggers: {}
+            })
+            
+            
+        // Create a document in the 'users' collection for the user
+        doing = 'Create doc'
+        const CreateDocRes = await axios.post(
+            `${process.env.APIURL}/_tenant/${process.env.TENANT}/_fabric/${process.env.FABRIC}/document/users`, {
+            _key: req.body.name,
+            about: req.body.about,
+            avatar: req.body.avatar,
+            active: req.body.active
+        })
+        
+        return res.status(202).send(`Created user with id ${CreateDocRes.data._id}. Everything is OK!`);
+
+    } catch( error) {
+        console.log(`*** Create User Request from ${req.connection.remoteAddress} to ${doing} "${req.body.name}" failed with error: ${error.message}`)
+        return res.status(501).send({what: `Failed to ${doing}`, err: error.message})
+    }
   });
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is listening on ${process.env.PORT}`)
 })
+
+app.post('/barfer/login', async (req, res) => {
+    console.log(`Request from ${req.connection.remoteAddress} to log in user "${req.body.name}"`)
+    
+    try {
+        // Get token
+        var JwtTokenRes = await axios.post(`${process.env.APIURL}/_open/auth`, {
+            tenant: process.env.TENANT,
+            password: req.body.passwd,
+            username: req.body.name
+        })
+        
+        // Return token and API url's
+        return res.send({
+            jwt: JwtTokenRes.data.jwt,
+            api: `${process.env.APIURL}/_tenant/${process.env.TENANT}/_fabric/${process.env.FABRIC}`,
+            wss: `wss://${host}/_ws/ws/v2/consumer/persistent/${process.env.TENANT}/c8global.${process.env.FABRIC}/${req.body.name}/1`
+        });
+
+
+    } catch(error) {
+        console.log(`*** Request from ${req.connection.remoteAddress} to get token "${req.body.name}" failed with error: ${error.message}`)
+        return res.status(500).send({what: 'Failed to obtain token', err: error.message})
+    }
+  });
+
+  app.post('/barfer/barf', async (req, res) => {
+    console.log(`Request from ${req.connection.remoteAddress} user "${req.body.barfer}" to barf`)
+
+    var barf = {
+        barfer: req.body.barfer,
+        barfType: req.body.type,
+        barf: req.body.text,
+        image: req.body.image,
+        timestamp: new Date().getTime()
+    }
+
+    try {
+        // TODO: Validate input
+
+        const CreateDocRes = await axios.post(
+            `${process.env.APIURL}/_tenant/${process.env.TENANT}/_fabric/${process.env.FABRIC}/document/barfs`, 
+            barf)
+
+        barf._key = CreateDocRes.data._key
+
+        await distibuteBarf(barf)
+
+        // Return status
+        return res.send({
+            status: "OK",
+            _key: CreateDocRes.data._key
+            });
+        
+    } catch(error) {
+        console.log(`*** Request from ${req.connection.remoteAddress} barfer "${req.body.name}" to barf failed with error: ${error.message}`)
+        return res.status(500).send({what: 'Failed to barf', err: error.message})
+    }
+  })
+
+async function distibuteBarf(barf) {
+    console.log(`Distributing barf ${barf._key}`)
+
+    const followersRes = await axios.post(
+        `${process.env.APIURL}/_tenant/${process.env.TENANT}/_fabric/${process.env.FABRIC}/cursor`, {
+            query: `with users
+            for follower in 1..1 inbound 'users/${barf.barfer}' follows
+                filter follower.active == true
+                return distinct follower._key`
+        })
+
+    // TODO: Do we need to handle followersRes.data.hasMore ?
+    for (ix in followersRes.data.result) {
+        var name = followersRes.data.result[ix]
+        console.log(` .. Barfing to ${name}`)
+
+        await sendToStream(barf, name)
+    }
+}
+
+async function sendToStream(barf, name) {
+    let promise = await new Promise((resolve, reject) => {
+
+        var streamUri = `wss://${host}/_ws/ws/v2/producer/persistent/${process.env.TENANT}/c8global.${process.env.FABRIC}/${name}`
+
+        console.log(`  ... Stream: ${streamUri}`)
+        
+        var ws = new WebSocket(
+            streamUri, {
+                headers: {
+                    Authorization: `Bearer ${process.env.JWT}`
+                }
+            })
+        
+        ws.on('open', () => {
+            console.log("..")
+
+            var payload = {
+                payload: Buffer.from(JSON.stringify(barf)).toString('base64'),
+            }
+
+            var pstr = JSON.stringify(payload)
+            console.log(`  ... Payload: ${pstr}`)
+
+            ws.send(pstr, (err) => {
+                if (err) {
+                    console.log(`Failed to barf to ${name}: ` + JSON.stringify(err))
+                    reject(JSON.stringify(err))
+                } else {
+                    resolve();
+                }
+            })
+        })
+
+        ws.on('message', (message) => {
+            console.log('received: %s', JSON.stringify(message));
+            ws.close()
+        });
+    })
+    return promise
+}
--- main.qml
+++ main.qml
@@ -1,6 +1,7 @@
 import QtQuick 2.12
 import QtQuick.Controls 2.12
 import Qt.labs.settings 1.0
+import QtWebSockets 1.1
 
 ApplicationWindow {
     id: mainWindow
@@ -8,6 +9,12 @@ ApplicationWindow {
     width: 640
     height: 480
     title: "Barfer"
+
+    property int appState: 0 // 0 init, 1 have token, 2 connecting, 3 connected
+    property string api: null
+    property string authHeader: null
+    property string wssUrl: null
+    property var keys: ([])
 
     // Remember the windows position and size
     Settings {
@@ -18,6 +25,7 @@ ApplicationWindow {
         property alias height: mainWindow.height
     }
 
+    // Remember the
     Settings {
         id: user
         property string name: null
@@ -25,13 +33,19 @@ ApplicationWindow {
     }
 
     Settings {
-        id: api
-        property string api: "https://jgaa-europe-west1.eng1.macrometa.io"
+        id: app
         property string service: "http://127.0.0.1:8080"
     }
 
+    ListModel {
+        id: feed
+    }
+
     Component.onCompleted: {
-        if (!user.name) {
+        if (user.name) {
+            next();
+        } else {
+            // Create a user account
             var component = Qt.createComponent("qrc:/CreateUserDlg.qml")
             if (component.status !== Component.Ready) {
                 if(component.status === Component.Error )
@@ -50,9 +64,9 @@ ApplicationWindow {
 
         ListView {
             width: parent.width
-            model: null
+            model: feed
             delegate: ItemDelegate {
-                text: "Item " + (index + 1)
+                text: `${when} ${barfer}: ${barf}`
                 width: parent.width
             }
         }
@@ -68,10 +82,10 @@ ApplicationWindow {
                     user.name = data.name
                     user.passwd = data.passwd
 
-                    // TODO: Refresh feed
+                    next();
 
                 } else {
-                    popup.text = qsTr("Error: ") + cli.responseText
+                    popup.text = qsTr("Error when creating user: ") + cli.responseText
                     popup.exitOnClose = true
                 }
 
@@ -80,10 +94,61 @@ ApplicationWindow {
             }
         }
 
-        cli.open('POST', `${api.service}/barfer/user`, true);
+        cli.open('POST', `${app.service}/barfer/user`, true);
         cli.setRequestHeader('Content-type', 'application/json');
         cli.send(JSON.stringify(data))
         busy.running = true
+    }
+
+    function login() {
+        var cli = new XMLHttpRequest();
+        cli.onreadystatechange = function() {
+            if (cli.readyState === XMLHttpRequest.DONE) {
+                if (cli.status === 200) {
+
+                    var data = JSON.parse(cli.responseText)
+
+                    mainWindow.api= data.api
+                    mainWindow.appState = 1
+                    mainWindow.authHeader = `Authorization: Bearer ${data.jwt}`
+                    mainWindow.wssUrl = data.wss
+
+                    next();
+
+                } else {
+                    popup.text = qsTr("Error getting login token: ") + cli.responseText
+                    popup.exitOnClose = true
+                    popup.open()
+                }
+
+                busy.running = false
+            }
+        }
+
+        cli.open('POST', `${app.service}/barfer/login`, true);
+        cli.setRequestHeader('Content-type', 'application/json');
+        cli.send(JSON.stringify({
+            name: user.name,
+            passwd: user.passwd
+        }))
+        busy.running = true
+    }
+
+    function next() {
+        if (appState === 0) {
+            login()
+        } else if (appState === 1) {
+            console.log("Activating websocket")
+            ws.active = false;
+            ws.active = true;
+            appState = 2
+        } else if (appState === 3) {
+            // We are connected to the feed
+        }
+    }
+
+    onAppStateChanged: {
+        console.log(`New appstate: ${appState}`)
     }
 
     BusyIndicator {
@@ -108,6 +173,52 @@ ApplicationWindow {
         onClosed: {
             if (exitOnClose) {
                 mainWindow.close()
+            }
+        }
+    }
+
+    WebSocket {
+        id: ws
+        url: mainWindow.wssUrl
+
+        onStatusChanged: {
+            console.log(`Wss Status: ${status}`)
+            switch(status) {
+            case WebSocket.Connecting:
+                console.log(`websocket connecting to "${url}"`)
+                busy.running = true
+                mainWindow.appState = 2
+                break
+            case WebSocket.Open:
+                busy.running = false
+                mainWindow.appState = 3
+                break
+            case WebSocket.Error:
+                console.log(`websocket error! ${errorString}`)
+                busy.running = false
+                if (mainWindow.appState === 3 || mainWindow.appState == 2) {
+                    mainWindow.appState = 1
+                }
+                break
+            case WebSocket.Closed:
+                ; // Do nothing
+            }
+            next()
+        }
+
+        onTextMessageReceived: (message) => {
+            console.log(`Wss onTextMessageReceived: ${message}`)
+            const msg = JSON.parse(message)
+            const payload = Qt.atob(msg.payload)
+            var barf = JSON.parse(payload)
+            barf.when = new Date(barf.timestamp).toLocaleString(Qt.locale('en'), Locale.ShortFormat);
+
+            // We keep all the keys we have loaded in 'keys to avoid
+            // duplicates when re-connecting. We could clear the feed
+            // when re-connecting as well, but that would cause flickering.
+            if (!keys.includes(barf._key)) {
+                keys.push(barf._key)
+                feed.insert(0, barf)
             }
         }
     }
